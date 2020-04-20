a2b5e2492c75f014abe629832365014bdb59e459
wafuwafu13 <mariobaske@i.softbank.jp>
2020-04-20 12:53:09 +0900

    ボタンを描写した

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import HomeTitle from '../elements/HomeTitle';
+import HomeButton from '../elements/HomeButton';
 
 const HomeScreen = () => {
     return(
         <View>
-            <HomeTitle />
+            <HomeTitle/>
+            <HomeButton type='start'>
+                スタート
+            </HomeButton>
+            <HomeButton type='setting'>
+                設定
+            </HomeButton>
         </View>
     )
 }
