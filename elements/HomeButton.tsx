a18da4a7d74ffcfc03cb76b627abb036260349de
wafuwafu13 <mariobaske@i.softbank.jp>
2020-04-20 14:38:41 +0900

    styleにwp.hpを設定

--- elements/HomeButton.tsx
+++ elements/HomeButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableHighlight } from 'react-native';
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 type Props = {
     type: string
@@ -9,26 +10,37 @@ type Props = {
 const Button = (props: Props) => {
 
     const { type } = props
+
     const blue = '#62D0E9'
     const red = '#FF4FC3'
-
+    
     let bgColor = (type === 'start') ? blue : red
 
     return(
-        <TouchableHighlight style={[styles.button, {backgroundColor: bgColor}]}>
-            <Text>{ props.children }</Text>
-        </TouchableHighlight>
+        <View style={styles.container}>
+            <TouchableHighlight style={[styles.button, {backgroundColor: bgColor}]}>
+                <Text style={styles.buttonText}>
+                    { props.children }
+                </Text>
+            </TouchableHighlight>
+        </View>
     )
 }
 
 export default Button;
 
 const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    },
     button: {
-       margin: 30,
-       top: 300,
-       height: 30,
-       justifyContent: 'center',
-       alignItems: 'center'
+        margin: hp('5%'),
+        width: wp('65%'),
+        height: hp('5%'),
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontSize: wp('6%')
     }
 })
