10f071dee4c1e09da144fb66250df069ba31343c
wafuwafu13 <mariobaske@i.softbank.jp>
2020-05-01 12:26:23 +0900

    play sound

--- components/StartScreen.tsx
+++ components/StartScreen.tsx
@@ -1,26 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Audio } from 'expo-av';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 const StartScreen = () => {
 
     const [count, setCount] = useState<any>(null);
 
+    async function playSound(){
+        try {
+            let soundObject = new Audio.Sound();
+            await soundObject.loadAsync(require('../assets/sounds/electronic-sound.mp3'))
+            await soundObject.playAsync();   
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
     function sleep(milliseconds: number) {
         return new Promise((resolve) => setTimeout(resolve, milliseconds));
-      }
+    }
       
     async function main() {
-      for (var i = 0; i < 10; i++) {
-          let oneDigitRandomNumber = Math.floor(Math.random() * 9) + 1
-          console.log(oneDigitRandomNumber)
-          setCount(oneDigitRandomNumber)
-          await sleep(1000);
-      }
+        for (var i = 0; i < 10; i++) {
+            let oneDigitRandomNumber = Math.floor(Math.random() * 9) + 1
+            console.log(oneDigitRandomNumber)
+            setCount(oneDigitRandomNumber)
+            playSound()
+            await sleep(1000);
+        }
     }
     
     useEffect(() => {
-      main()
+        main()
     },[])
     
     return(
