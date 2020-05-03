ec969c423735dec572d735c8a04f3e83db8bc96c
wafuwafu13 <mariobaske@i.softbank.jp>
2020-05-03 18:09:39 +0900

    バリデーション

--- components/SheetModal.tsx
+++ components/SheetModal.tsx
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableHighlight, TextInput } from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 type Props = {
     onPress: any
-    currentValue:　any
     changeValue: any
 }
 
 const SheetModal = (props: Props) => {
 
-    const { onPress, currentValue, changeValue } = props
+    const { onPress, changeValue } = props
+
+    const [isValidated, setIsValidated] = useState<boolean>(false)
+
+    let validateList: number[] = []
+    for (let i = 1; i <= 100; i++) {
+        validateList.push(i)
+    } 
+
+    const handleSheet = (number: string) => {
+        const input_number = Number(number)
+        const index = validateList.findIndex(number => number == input_number)
+        if (index === -1) {
+            setIsValidated(false)
+        } else {
+            setIsValidated(true)
+            changeValue(number)
+        }
+    }
 
     return(
         <View style={styles.modal}>
             <Text style={styles.text}>枚数を入力してください。</Text>
             <TextInput
               keyboardType = 'numeric'
-              onChangeText={number => changeValue(number)}
-              value={currentValue}
+              onChangeText={handleSheet}
+              maxLength={3}
               style={styles.input} 
             />
-            <TouchableHighlight
-              onPress={onPress}
-            >
-                <Text>決定</Text>
-            </TouchableHighlight>
+            { isValidated && (
+                <TouchableHighlight
+                  onPress={onPress}
+                >
+                    <Text>決定</Text>
+                </TouchableHighlight>
+            )}
         </View>
     )
 }
