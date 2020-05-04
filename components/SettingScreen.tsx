0e57c2895ca0e05c744c9249c1826603fba2c944
wafuwafu13 <mariobaske@i.softbank.jp>
2020-05-04 13:01:24 +0900

    setting modal layout

--- components/SettingScreen.tsx
+++ components/SettingScreen.tsx
@@ -39,7 +39,7 @@ const SettingScreen = ({ navigation }: any ) => {
         <View style={styles.container}>
             <Header
               leftComponent={<BackArrow onPress={() => navigation.navigate('Home')} />}
-              centerComponent={{ text: '設定', style: { color: '#fff', fontSize: wp('5%')} }}
+              centerComponent={{ text: '設 定', style: { color: '#fff', fontSize: wp('5%')} }}
               backgroundColor='#7244F4'
             />
             <View style={styles.wrapper}>
